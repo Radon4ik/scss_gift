d1cc6330a575e70ec9c69b8f986096ed45ff8bcd
Radon4ik <Loboda.Radimir@gmail.com>
2022-06-22 23:33:20 +0300

    added projects files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './index.scss';
-
-async function check() {
-    await fetch('https://google.com');
-
-}
-
-check().then(() => {
-    console.log("success");
-}).catch(() => {
-    console.log('error');
-})
